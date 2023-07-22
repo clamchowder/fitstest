0d463264af5179dc516defcc78d5c00e39623b41
lamchester@gmail.com <lamchester@gmail.com>
2023-07-22 03:33:19 +0000

    fix compile

--- fitstest.h
+++ fitstest.h
@@ -7,9 +7,9 @@
 
 #define MAX_SOURCE_SIZE (0x100000)
 
-cl_device_id selected_device_id;
-cl_platform_id selected_platform_id; 
-cl_context context; 
+extern cl_device_id selected_device_id;
+extern cl_platform_id selected_platform_id; 
+extern cl_context context; 
 
 void get_context_from_user(int platform_index, int device_index); 
 double *calculate_potential(double *data, long x_len, long y_len, int fp32); 
